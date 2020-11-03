e606d51776ac2b4ab1b41a0bde8810fde1dc968b
Nezzar KEFIF <kefifnezzar@gmail.com>
2020-11-03 11:55:12 +0100

    adding quizz questions

---
+++ index.js
@@ -0,0 +1,64 @@
+console.log(`QUIZZ - 1`)
+// Créer une branche quizz1/[PRENOM]-[NOM]
+
+
+// 1 - Write a function that capitalizes the first letter of a word
+// arguments: word
+
+
+
+// 2 - Write a function that takes firstname and lastname and returns the initials
+// arguments: firstName, lastName
+
+
+
+// 3 - Write a function that returns the date in this specific format: "Nous sommes le 22/10 et il est 16h00"
+// arguments: 
+
+function whatIsToday() {
+  const today = new Date()
+
+  // TODO
+}
+
+// 4 - Write a function that converts a temperature in Celsius to Fahrenheit
+// arguments: tempInCelsius
+
+
+
+// 5 - Write a function that simulates a dice roll and returns a random number between 1 and 6 everytime it is run.
+// arguments: -
+// tips: utiliser l'objet Math
+
+
+
+// Bonus: 6 - Write a function that takes the name of a book and returns if it is available, if it was borrowed or if the library doesn't have it.
+const library = {
+  frankenstein: {
+    available: false
+  },
+  dune: {
+    available: true
+  },
+  "1984": {
+    available: true
+  },
+  warOfTheWorlds: {
+    available: true
+  },
+  theHungerGames: {
+    available: false
+  },
+  braveNewWorld: {
+    available: false
+  },
+  theMartian: {
+    available: true
+  },
+  iRobot: {
+    available: true
+  },
+}
+
+
+// Pusher
